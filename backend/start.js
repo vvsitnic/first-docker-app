5449f98030657d6c7d754ff11560cf7f10cf9daf
vvsitnic <asleepcarpenterdev@gmail.com>
2026-03-20 23:08:48 +0200

    Changed backend and frontend. separated nginx and compose files into dev and prod.

--- backend/start.js
+++ backend/start.js
@@ -1,6 +1,8 @@
-const express = require("express");
-const cors = require("cors");
-const mysql = require("mysql2");
+import express from "express";
+import cors from "cors";
+import mysql from "mysql2/promise";
+import cookieParser from "cookie-parser";
+import "dotenv/config";
 
 const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
@@ -9,25 +11,62 @@ const pool = mysql.createPool({
   database: process.env.MYSQL_DATABASE,
 });
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
-app.use(cors());
+const corsOptions = {
+  credentials: true,
+  origin: process.env.ADDRESS,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello World!" });
 });
 
-app.get("/time", (req, res) => {
-  pool.query("SELECT NOW() as curr_date_and_time", function (err, results) {
+app.get("/get-time", async (req, res) => {
+  try {
+    const [results] = await pool.query("SELECT NOW() as curr_date_and_time");
+
+    res.json({
+      message: "success",
+      time: results[0]["curr_date_and_time"],
+    });
+  } catch (err) {
     if (err instanceof Error) {
       res.status(500).json({ message: "Something when wrong" });
-    } else {
-      res.json({
-        message: "success",
-        time: results[0]["curr_date_and_time"],
-      });
+      return;
     }
+  }
+});
+
+app.get("/get-cookie", (req, res) => {
+  res.cookie("cookie-token", "someToken", {
+    httpOnly: true,
+    sameSite: "none",
+    secure: true,
+    maxAge: 3600000,
   });
+  res.json({
+    message: "success",
+  });
+});
+
+app.post("/send-cookie", (req, res) => {
+  const token = req.cookies["cookie-token"];
+
+  if (token == null) {
+    return res.sendStatus(500);
+  }
+
+  if (token == "someToken") {
+    res.json({
+      message: "success",
+    });
+  }
 });
 
 app.listen(port, () => {
--- frontend/src/script.js
+++ frontend/src/script.js
@@ -1,13 +1,43 @@
-const button = document.getElementById("time-button");
-const text = document.getElementById("time-text");
+const getTimeBtn = document.getElementById("get-time");
+const getCookieBtn = document.getElementById("get-cookie");
+const sendCookieBtn = document.getElementById("send-cookie");
 
-button.addEventListener("mousedown", async () => {
-  const response = await fetch(`${process.env.BACKEND_API}/time`);
-  if (!response.ok) {
-    console.error("An unexpected error occured. The response was not ok");
-    return;
-  }
+getTimeBtn.addEventListener("click", async () => {
+  const response = await fetch(`${process.env.BACKEND_API}/get-time`, {
+    credentials: "include",
+  });
 
   const data = await response.json();
-  text.textContent = data.time;
+  if (!response.ok) {
+    console.error(data.message || "An unexpected error occured.");
+  } else {
+    console.log(data.time);
+  }
+});
+
+getCookieBtn.addEventListener("click", async () => {
+  const response = await fetch(`${process.env.BACKEND_API}/get-cookie`, {
+    credentials: "include",
+  });
+
+  const data = await response.json();
+  if (!response.ok) {
+    console.error(data.message || "An unexpected error occured.");
+  } else {
+    console.log("Cookies received");
+  }
+});
+
+sendCookieBtn.addEventListener("click", async () => {
+  const response = await fetch(`${process.env.BACKEND_API}/send-cookie`, {
+    method: "POST",
+    credentials: "include",
+  });
+
+  const data = await response.json();
+  if (!response.ok) {
+    console.error(data.message || "An unexpected error occured.");
+  } else {
+    console.log(data.message);
+  }
 });
